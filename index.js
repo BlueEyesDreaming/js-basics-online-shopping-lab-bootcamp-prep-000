6135eeb693b7df48db561622993a40b24ee69129
Ashton Reed <dreamergirl411@gmail.com>
2018-01-09 01:25:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  // var obj = Object.assign({}, cart, { itemName: itemPrice })
+  // console.log()
 }
 
 function viewCart() {
